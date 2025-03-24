78cadfb186c1baa8e1f54e485d26aa7624a77996
Yan Sun <yan@pensando.io>
2025-03-24 16:09:21 -0700

    Refactor IsOpenShift function based on RedHat suggestion (#503)

--- internal/utils.go
+++ internal/utils.go
@@ -17,15 +17,25 @@ limitations under the License.
 package utils
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
-	amdv1alpha1 "github.com/ROCm/gpu-operator/api/v1alpha1"
+	"github.com/go-logr/logr"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	amdv1alpha1 "github.com/ROCm/gpu-operator/api/v1alpha1"
+	"github.com/ROCm/gpu-operator/internal/cmd"
 )
 
 const (
 	defaultOcDriversVersion = "6.2.2"
+	openShiftNodeLabel      = "node.openshift.io/os_id"
+	NodeFeatureLabelAmdGpu  = "feature.node.kubernetes.io/amd-gpu"
+	NodeFeatureLabelAmdVGpu = "feature.node.kubernetes.io/amd-vgpu"
 )
 
 func GetDriverVersion(node v1.Node, deviceConfig amdv1alpha1.DeviceConfig) (string, error) {
@@ -76,4 +86,40 @@ func UbuntuDefaultDriverVersionsMapper(fullImageStr string) (string, error) {
 		return "6.1.3", nil // due to a known ROCM issue, 6.2 unload + load back may cause system reboot, let's use 6.1.3 as default
 	}
 	return "", fmt.Errorf("invalid ubuntu version, should be one of [20.04, 22.04]")
+}
+
+func HasNodeLabelKey(node v1.Node, labelKey string) bool {
+	for k := range node.Labels {
+		if k == labelKey {
+			return true
+		}
+	}
+	return false
+}
+
+func IsOpenShift(logger logr.Logger) bool {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		cmd.FatalError(logger, err, "unable to get cluster config")
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		cmd.FatalError(logger, err, "unable to create cluster clientset")
+	}
+	// Check for OpenShift-specific labels on nodes
+	nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		cmd.FatalError(logger, err, "unable to list nodes")
+	}
+
+	isOpenShift := false
+	for _, node := range nodes.Items {
+		if _, exists := node.Labels[openShiftNodeLabel]; exists {
+			isOpenShift = true
+			break
+		}
+	}
+	logger.Info(fmt.Sprintf("IsOpenShift: %+v", isOpenShift))
+	return isOpenShift
 }
