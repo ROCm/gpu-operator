59e016a2d8e5aad2db42157ec1f20afcdc095feb
Farshad Ghodsian <47931571+farshadghodsian@users.noreply.github.com>
2025-04-09 11:42:41 -0400

    Updated ReadTheDocs conf to support copy code block button

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,27 @@
 """Configuration file for the Sphinx documentation builder."""
+import os
 
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "instinct.docs.amd.com")
+html_context = {}
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
 external_projects_local_file = "projects.yaml"
 external_projects_remote_repository = ""
 external_projects = ["amd-gpu-operator"]
 external_projects_current_project = "amd-gpu-operator"
 
-project = "AMD Instinct Documentation"
+project = "AMD GPU Operator"
 version = "1.2.0"
 release = version
-html_title = f"AMD GPU Operator {version}"
+html_title = f"{project} {version}"
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved."
 
 # Required settings
 html_theme = "rocm_docs_theme"
 html_theme_options = {
-    "flavor": "instinct"
+    "flavor": "instinct",
+    "link_main_doc": True,
     # Add any additional theme options here
 }
 extensions = ["rocm_docs"]
