29082457d459bf0e06b817729fe70d68360e9057
Yan Sun <Yan.Sun3@amd.com>
2025-05-08 10:55:50 -0700

    Change default exporter and test runner image tag back to v1.2.1 (#662)

--- internal/testrunner/testrunner.go
+++ internal/testrunner/testrunner.go
@@ -49,7 +49,7 @@ import (
 )
 
 const (
-	defaultTestRunnerImage       = "docker.io/rocm/test-runner:v1.2.2-beta.0"
+	defaultTestRunnerImage       = "docker.io/rocm/test-runner:v1.2.1-beta.0"
 	defaultInitContainerImage    = "busybox:1.36"
 	TestRunnerName               = "test-runner"
 	defaultSAName                = "amd-gpu-operator-test-runner"
