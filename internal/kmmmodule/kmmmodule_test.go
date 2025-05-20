c6dd9b4a8938c1377431bf3fc4505e542889b3b9
vm <sriramr2230@gmail.com>
2025-05-20 12:15:14 +0000

    dcm taint toleration from GPU Operator to KMM Operator

--- internal/kmmmodule/kmmmodule_test.go
+++ internal/kmmmodule/kmmmodule_test.go
@@ -127,6 +127,12 @@ var _ = Describe("setKMMModuleLoader", func() {
 				Operator: v1.TolerationOpEqual,
 				Effect:   v1.TaintEffectNoSchedule,
 			},
+			{
+				Key:      "amd-dcm",
+				Value:    "up",
+				Operator: v1.TolerationOpEqual,
+				Effect:   v1.TaintEffectNoExecute,
+			},
 		}
 
 		err = setKMMModuleLoader(context.TODO(), &mod, &input, false, testNodeList)
@@ -190,6 +196,12 @@ var _ = Describe("setKMMModuleLoader", func() {
 				Value:    "true",
 				Operator: v1.TolerationOpEqual,
 				Effect:   v1.TaintEffectNoSchedule,
+			},
+			{
+				Key:      "amd-dcm",
+				Value:    "up",
+				Operator: v1.TolerationOpEqual,
+				Effect:   v1.TaintEffectNoExecute,
 			},
 		}
 
